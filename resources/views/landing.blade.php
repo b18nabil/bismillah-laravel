438a90d77018b2017bbb355dce1829ed162238ef
b18nabil <b18nabil@gmail.com>
2022-12-13 18:16:18 +0700

    first commit

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MahasisWork</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+
+
+  </head>
+  <body>
+    
+    <div class="container-fluid my-5">
+    <nav class="navbar navbar-expand-lg bg-light custom-nav bg-transparent">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">MahasisWork</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Become a freelance</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link btn btn-primary login-button" href="#">Login</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+    
+
+    <section class="hero">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-6 konten-kiri">
+              <span class="hero-text">Starting <span class="freelance">freelance</span> as easy</span><br>
+              <span class="hero-text">as make a <span class="sosmed">Social Media</span></span>
+
+              <div class="input-group mb-3 search-bar">
+                <input type="text" class="form-control" placeholder="try 'english teacher'"aria-describedby="basic-addon2">
+                <span class="input-group-text btn btn-primary search-button" id="basic-addon2">Search</span>
+              </div>
+            </div>
+
+
+            <div class="col-6">
+              <img src="img/hero1.svg" alt="" style="margin-top: 7px; margin-left: 12em;">
+            </div>
+          </div>
+        </div>
+    </section>
+
+    <section class="service">
+      <div class="container-fluid">
+        <h1 class="service-title">Popular service</h1>
+        <div class="row produk">
+          <div class="col-3">
+            <div class="card text-bg-dark pil-produk">
+              <img src="img/Mentor.png" class="card-img" alt="...">
+              <div class="card-img-overlay">
+                <h5 class="card-title">Course <br> mentor</h5>
+              </div>
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="card text-bg-dark pil-produk">
+              <img src="img/Design.png" class="card-img" alt="...">
+              <div class="card-img-overlay">
+                <h5 class="card-title">Graphic Design</h5>
+              </div>
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="card text-bg-dark pil-produk">
+              <img src="img/translation.png" class="card-img" alt="...">
+              <div class="card-img-overlay">
+                <h5 class="card-title">Translation</h5>
+              </div>
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="card text-bg-dark pil-produk">
+              <img src="img/writing.png" class="card-img" alt="...">
+              <div class="card-img-overlay">
+                <h5 class="card-title">Writing</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="choose">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-6 konten-kiri">
+            <h1 class="choose-title">Why choose <br> <span class="mahasiswork">Mahasiswork</span>?</h1>
+
+            <br>
+
+            <ul class="reason" style="list-style: none;">
+              <li class="list">
+                <img src="img/check.svg" alt="">
+                <span>Ease of Use</span>
+                <br>
+                <p class="desc">Starting work and hire your worker as easy as following people on social media</p>
+              </li>
+              <li class="list">
+                <img src="img/check.svg" alt="">
+                <span>The best for every budget</span> 
+                <br>
+                <p class="desc">Find your right price in every service. No hourly rates, just project based pricing</p>
+              </li>
+              <li class="list">
+                <img src="img/check.svg" alt="">
+                <span> 24/7 support</span>
+                <br>
+                <p class="desc">Any question? Our support team gonna help you anywhere, anytime</p>
+              </li>
+            </ul>
+          
+          </div>
+          <div class="col-6 konten-kanan">
+            <img src="img/why choose.png" alt="" style="margin-top: 206px; margin-left: 10em;">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="commit">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-6 konten-kiri">
+            <img src="img/our commit.png" alt="" style="margin-left: 120px;">
+          </div>
+          <div class="col-6 konten-kanan">
+            <h1 class="commit-title">Our commitment</h1>
+            <h3 class="desc">“We make <span class="pink">MahasisWork</span>  as a place for student to <span class="pink">start their own careers</span>, and <span class="pink">develop their skills</span>, so that they can achieve their dreams careers”</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="footer">
+      <div class="container-fluid">
+        <div class="row upper">
+          <div class="col-3 title">
+            <span class="mahasiswork">MahasisWork</span> <br>
+            <img src="img/instagram.svg" class="instagram" alt="">
+            <img src="img/twitter.svg" class="twitter" alt="">
+          </div>
+          <div class="col-3 menu-1">
+            <span class="categories">Categories</span>
+            <ul>
+              <li class="cat-list"><a class="cat-nav" href="">Mentor course</a></li>
+              <li class="cat-list"><a class="cat-nav" href="">Graphic Design</a></li>
+              <li class="cat-list"><a class="cat-nav" href="">Translation</a></li>
+              <li class="cat-list"><a class="cat-nav" href="">Writing</a></li>
+              <li class="cat-list"><a class="cat-nav" href="">Photo editing</a></li>
+              <li class="cat-list"><a class="cat-nav" href="">Video editing</a></li>
+              <li class="cat-list"><a class="cat-nav" href="">Programming & Tech</a></li>
+              <li class="cat-list"><a class="cat-nav" href="">Data</a></li>
+            </ul>
+          </div>
+          <div class="col-3 menu-2">
+            <span class="community">Community</span>
+            <ul>
+              <li class="comm-list"><a class="comm-nav" href="">Blog</a></li>
+              <li class="comm-list"><a class="comm-nav" href="">Event</a></li>
+              <li class="comm-list"><a class="comm-nav" href="">Forums</a></li>
+              <li class="comm-list"><a class="comm-nav" href="">Invite a friends</a></li>
+            </ul>
+          </div>
+          <div class="col-3 menu-3">
+            <span class="support">Support</span>
+            <ul>
+              <li class="supp-list"><a href="" class="supp-nav">Help & Support</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row lower">
+          <span class="copyright">Copyright <a href="">MahasisWork</a> 2022</span>
+        </div>
+      </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
